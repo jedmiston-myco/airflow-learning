f027b252e065df480f8700b97b7f518dd7e08583
John Edmiston <john@mycoworks.com>
2020-09-02 12:49:48 -0700

    airflow-cloud-run -- add ref commit comment for env vars

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,8 @@
 
 echo "entry point command" "$1"
 
+# this setup of the internal env vars in entrypoint script taken
+# from https://github.com/puckel/docker-airflow @ bed777970caa3e555ef618d84be07404438c27e3
 : "${PORT:=5000}"
 : "${AIRFLOW__WEBSERVER__WEB_SERVER_PORT=${PORT}}"
 
